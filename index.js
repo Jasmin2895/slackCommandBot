8efde1e5c9557d0dbaeb0ad674daed9f4622bad9
Jasmin Virdi <jasmin.virdi@upgrad.com>
2019-11-11 01:40:09 +0530

    add slack config code on get

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/slack", (req, res) => {
     console.log("get request")
+    let data = {
+        form: { 
+            client_id: process.env.SLACK_CLIENT_ID, 
+            client_secret: process.env.SLACK_CLIENT_SECRET, 
+            code: req.query.code 
+        }
+    }; 
+    request.post('https://slack.com/api/oauth.access', data, function (error, response, body) { 
+    if (!error && response.statusCode == 200) { 
+      // You are done. 
+      // If you want to get team info, you need to get the token here 
+        let token = JSON.parse(body).access_token; // Auth token
+        request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) { 
+        if (!error && response.statusCode == 200) { 
+        let team = JSON.parse(body).team.domain; 
+        res.redirect('http://' +team+ '.slack.com'); 
+  } 
+});  
+    }
     res.send("Hello slackBot");
 })
-app.post('/httpstatus', (req, res) => {
+app.post('/', (req, res) => {
     console.log("post request");
     let text = req.body.text;
     if(! /^\d+$/.test(q.text)) { // not a digit 
