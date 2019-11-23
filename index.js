2f45b1a34821a1168e366662c2bee968c9f69029
Jasmin Virdi <jasmin.virdi@upgrad.com>
2019-11-24 02:28:44 +0530

    changing index js file to run slack bot locally

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
-'use strict';
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+'use strict'; 
+const express = require('express'); 
+const bodyParser = require('body-parser'); 
+const request = require('request'); 
+const app = express(); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+const server = app.listen(3000, () => { console.log('Express server   listening on port %d in %s mode', server.address().port, app.settings.env); });
 
-app.get("/slack", (req, res) => {
-    console.log("get request")
-    let data = {
-        form: { 
-            client_id: process.env.SLACK_CLIENT_ID, 
-            client_secret: process.env.SLACK_CLIENT_SECRET, 
-            code: req.query.code 
-        }
-    }; 
-    request.post('https://slack.com/api/oauth.access', data, function (error, response, body) { 
-    if (!error && response.statusCode == 200) { 
-      // You are done. 
-      // If you want to get team info, you need to get the token here 
-        let token = JSON.parse(body).access_token; // Auth token
-        request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) { 
-        if (!error && response.statusCode == 200) { 
-        let team = JSON.parse(body).team.domain; 
-        res.redirect('http://' +team+ '.slack.com'); 
-  } 
-});  
-    }
-    res.send("Hello slackBot");
-})
-app.post('/', (req, res) => {
-    console.log("post request");
-    let text = req.body.text;
-    if(! /^\d+$/.test(q.text)) { // not a digit 
-        res.send('U R DOIN IT WRONG. Enter a status code like 200!');   return; 
-    }
-    else {
-        let data = {
-            response_type: "in_channel",
-            text: "302: Found",
-            attachments: [{
-                image_url: "https://http.cat/302.jpg"
-            }]
-        }
-        res.send(data);
-    }
+app.get('/', (req, res) => { 
+    // console.log("req", req);
+    res.send("Hello world!");
+    return;
 });
-const server = app.listen(3000, () => {
-    console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
+app.post('/', (req, res) => { 
+    let text = req.body.text; 
+    console.log("text", text);
+    let data = { 
+    response_type: 'in_channel', // public to the channel 
+    text: '302: Found', 
+    attachments:[ { 
+        image_url: 'https://http.cat/302.jpg' 
+    } ]}; 
+    // res.json(data);
+    console.log("res data", res.json(data));
+    res.send("hello from salck alert");
+    // implement your bot here ... 
+    //     if(! /^\d+$/.test(q.text)) { // not a digit 
+    //   res.send('U R DOIN IT WRONG. Enter a status code like 200!');   return; 
+    // }
+});
+app.get('/slack', function (req, res) {
+    let data = {
+        form: {
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET,
+            code: req.query.code
+        }
+    };
+    request.post('https://slack.com/api/team.info', { form: { token: token } }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let team = JSON.parse(body).team.domain;
+            res.redirect('http://' + team + '.slack.com');
+        }
+    })
 });
